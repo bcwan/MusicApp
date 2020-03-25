1f39dd58054ebbb49597f311c7cdbe25528c6414
bcwan <bcwan8@gmail.com>
2020-03-25 14:24:40 -0700

    added method to find by credentials

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
     #makes sure we have a token
     after_initialize :ensure_session_token
-    
+
     #generate a cookie
     def self.generate_session_token
         SecureRandom::urlsafe_base64(16)
@@ -35,5 +35,13 @@ class User < ApplicationRecord
         BCrypt::Password.new(self.password_digest).is_password?(password)
     end
 
+    def self.find_by_credentials(email, password)
+        #trys to find by email
+        user = User.find_by(email: email)
+        #if no user, then return nil
+        return nil unless user
+        #unsalts and checks to see if hash matches password string
+        user.is_password?(password)
+    end
 
 end
