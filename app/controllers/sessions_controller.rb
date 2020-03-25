0f789fe04457f35ea9f754133b10749239bf8611
bcwan <bcwan8@gmail.com>
2020-03-25 15:33:09 -0700

    adding new routes.rb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,18 @@
 class SessionsController < ApplicationController
+    def create
+        user = User.find_by_credentials(params[:user][:email], params[:user][:password])
+
+        if user.nil?
+            flash.now[:errors] = ["Incorrect email/password combo"]
+            render :new
+        else  
+            login_user!(user)
+            redirect_to user_url
+        end
+
+    end
+
     
+
+
 end
