9229a776989571558a25b829efd272ebf1826c73
bcwan <bcwan8@gmail.com>
2020-03-25 14:02:08 -0700

    adding to user model cookie session token

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,32 @@
 class User < ApplicationRecord
     validates :email, :session_token, presence: true, uniqueness: true
+    validates :password_digest, presence: { message: 'Please enter password' }
+
+    attr_reader :password
+
+    generate a cookie
+    def self.generate_session_token
+        SecureRandom::urlsafe_base64(16)
+    end
+
+    def reset_session_token!
+        #reset a new session token
+        self.session_token = User.generate_session_token
+        #attempt to save
+        self.save!
+        #return new session token
+        self.session_token
+    end
+
+    def ensure_session_token
+        self.session_token ||= User.generate_session_token
+    end
+
+    def password=(password)
+        @password = password
+        #salting password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+
 end
