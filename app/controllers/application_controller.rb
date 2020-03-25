c1c755fc157df8ed30f0c734df64e47e8b3876ee
bcwan <bcwan8@gmail.com>
2020-03-25 16:30:41 -0700

    application_controller edited

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,21 @@
 class ApplicationController < ActionController::Base
-    
+    def current_user
+        return nil unless session[:session_token]
+        @current_user ||= User.find_by(session_token: session[:session_token])
+    end
+
+    #if current_user has no instance of user, then we are not logged in
+    def logged_in?
+        !current_user.nil?
+    end
+
+    #reset the user's session token and cookie
+    def login_user!(user)
+        session[:session_token] = user.reset_session_token!
+    end
+
+    def logout_user!
+        current_user.reset_session_token!
+        session[:session_token] = nil
+    end
 end
