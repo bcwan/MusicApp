904100bc6dde97ebed4933ad888f10e71fc40240
bcwan <bcwan8@gmail.com>
2020-03-25 14:12:05 -0700

    added is_password? validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,5 +28,9 @@ class User < ApplicationRecord
         self.password_digest = BCrypt::Password.create(password)
     end
 
+    def is_password?(password)
+        BCrypt::Password.new(self.password_digest).is_password?(password)
+    end
+
 
 end
